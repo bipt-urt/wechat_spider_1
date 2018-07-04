720a95ff48942247473670335f4357f87b2be9e3
gh <1144042303@qq.com>
2018-07-04 20:23:25 +0800

    得到了所有群组

--- gh/gh.py
+++ gh/gh.py
@@ -4,8 +4,15 @@ import urllib
 import http.cookiejar
 import json
 import csv
+import io
+import sys
+import random
+
+print('start')
+#改变标准输出的默认编码 
 
 
+print('ok')
 def main():
 	cj = http.cookiejar.CookieJar()
 	opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
@@ -61,6 +68,7 @@ ticket=ticket[0]
 ticket=ticket.split("<pass_ticket>")
 ticket=ticket[1]
 print('=========================开始获取您的各种信息=========================')
+print('=========================您即将交出的所有信息=========================')
 #print(ticket)
 #找到ticket
 
@@ -78,7 +86,6 @@ Uin=response3.split("</wxuin>")
 Uin=Uin[0]
 Uin=Uin.split("<wxuin>")
 Uin=Uin[1]
-print('=========================您即将交出的所有信息=========================')
 #print(Uin)
 #找到Uin
 
@@ -94,9 +101,15 @@ print('==========================………………………………………�
 
 list4='https://wx2.qq.com/cgi-bin/mmwebwx-bin/webwxgetcontact?skey='+Skey+'&r=-1493979626&lang=zh_CN&pass_ticket='+ticket
 #print(list4)
-postData={'BaseRequest':{"Uin":Uin,"Sid":Sid,"Skey":Skey,"DeviceID":"e899027140826785"}}
-response4= urllib.request.urlopen(list4, json.dumps(postData).encode('utf-8')).read()
+postData1={'BaseRequest':{"Uin":Uin,
+						"Sid":Sid,
+						"Skey":Skey,
+						"DeviceID":"e899027140826785"}}
+response4= urllib.request.urlopen(list4, json.dumps(postData1).encode('utf-8')).read()
 print('======================================================================')
+
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='gb18030')
 with open("data.txt","wb") as f:
 	f.write(response4)
 with open("data.txt", "r", encoding="utf-8") as f:
@@ -104,10 +117,93 @@ with open("data.txt", "r", encoding="utf-8") as f:
 	target=json.loads(content)
 	#print(target.keys())
 datalist=target['MemberList']
-with open("data.csv","w",newline="") as datacsv:
+#print(content)
+#print(type(datalist))
+#print(datalist)
+with open("data.csv","w",encoding="gb18030",newline="") as datacsv:
 	for data1 in datalist:
 		for data2 in data1:
-			data1[data2]=str(data1[data2])
-		print(data1['NickName'])
-		datacsv.write(data1['UserName']+" "+data1['NickName']+data1['RemarkName']+"\n")
+			data1[data2]=str(data1[data2]).replace('\n',"")
+		#print(data1['NickName'])
+		datacsv.write(data1['UserName']+','+data1['NickName']+','+data1['RemarkName']+','+data1['Signature']+','+data1['Province']+','+data1['SnsFlag']+"\n")
+print('=========================您已经交出的所有信息=========================')
 #获得用户好友信息
+
+
+Name=input('输入联系人：')
+message=input('输入消息：')
+result={}
+for element in datalist:
+	if element["RemarkName"]==Name:
+		result=element
+		break
+	if element["NickName"]==Name:
+		result=element
+		break
+choose=result['UserName']
+#print(choose)
+#print(type(choose))
+#输入发送对象和发送信息
+
+
+result2={}
+list5='https://wx2.qq.com/cgi-bin/mmwebwx-bin/webwxinit?r=-1671848414'
+postData2={"BaseRequest":{"Uin":Uin,
+						"Sid":Sid,
+						"Skey":Skey,
+						"DeviceID":"e742471654104946"}}
+response5= urllib.request.urlopen(list5, json.dumps(postData2).encode('utf-8')).read()
+with open("mine.txt","wb") as f:
+	f.write(response5)
+with open("mine.txt", "r", encoding="utf-8") as f:
+	content=f.read()
+	target=json.loads(content)
+result2=target['User']
+#print(result2)
+mine=result2['UserName']
+#print(mine)
+
+
+id=random.random()
+id=str(id)[2:] + '1'
+
+list6='https://wx2.qq.com/cgi-bin/mmwebwx-bin/webwxsendmsg?lang=zh_CN&pass_ticket='+ticket
+postData3={'BaseRequest':
+						{"Uin":Uin,
+						"Sid":Sid,
+						"Skey":Skey,
+						"DeviceID":"e910132017991627"},
+				"Msg":{"Type":1,
+					"Content":message,
+					"FromUserName":mine,
+					"ToUserName":choose,
+					"LocalID":id,
+					"ClientMsgId":id},
+					"Scene":0}
+send_data=json.dumps(postData3,ensure_ascii=False)#转成能发中文
+send_bytes=bytes(send_data,'utf-8').replace(b' ',b'')
+html_send=urllib.request.Request(list6,send_bytes)
+response6=urllib.request.urlopen(html_send)
+
+#urllib.request.urlopen(list6, bytes(json.dumps(postData).encode('utf-8'))).read()
+
+list7 = []
+with open('data.csv','r', encoding="gb18030") as f:
+	for line in f:
+		row = []
+		if line.find("@@") == -1:
+			continue
+		line = line.split(",")
+		for element in line:
+			row.append(element)
+		list7.append(row)
+#print(list7)
+
+
+with open("group.csv","w",encoding="gb18030",newline="") as groupcsv:
+	for i in list7:
+		response7=i
+		response7=str(response7)
+		print(response7)
+		groupcsv.write(response7+'\n')
+#将群组写入csv
